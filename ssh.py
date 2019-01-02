94623e61fd2d7ff828d5dd9cebe166eacdce242b
sunleoyao <sunleoyao@qq.com>
2019-01-02 17:00:45 +0800

    创建

---
+++ ssh.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import paramiko
+#创建SSH对象
+ssh = paramiko.SSHClient()
+#把要连接的机器添加到known_hosts文件中
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#连接服务器
+ssh.connect(hostname='192.168.122.134', port=22, username='root', password='rootroot')
+cmd = 'su - root'
+#cmd = 'ls -l;ifconfig'       #多个命令用;隔开
+stdin, stdout, stderr = ssh.exec_command(cmd)
+cmd = 'rootroot'
+stdin, stdout, stderr = ssh.exec_command(cmd)
+cmd = 'whoami'
+stdin, stdout, stderr = ssh.exec_command(cmd)
+result = stdout.read()
+if not result:
+    result = stderr.read()
+ssh.close()
+print(result.decode())
