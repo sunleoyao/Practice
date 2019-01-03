fb9dc07d0eac39285afc575d5cde1d234da2c6a7
sunleoyao <sunleoyao@qq.com>
2019-01-03 15:35:11 +0800

    创建if练习

---
+++ study/if.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*
+name=input("Please input your name:\n")
+print("Your name is:"+name)
+if name=='aaa':
+    print("a")
+elif name=='bbb':
+    print('b')
+else:
+    print("c")
