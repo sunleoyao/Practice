23183d04f0f01809ecbb4714f77c2a6dc1bcaf7c
sunleoyao <sunleoyao@qq.com>
2019-01-04 09:36:38 +0800

    Update dict.py

--- study/dict.py
+++ study/dict.py
@@ -4,16 +4,16 @@ print(dict_test['a'])
 print(dict_test.get('a'))
 print(dict_test.get('aa'))
 print('aa' in dict_test)
-
+###增
 dict_test['d'] = 4
 print(dict_test)
-
+###改
 dict_test['d'] = 666
 print(dict_test)
-
+###改增
 dict_test_temp = {'dd': 888}
 dict_test.update(dict_test_temp)
 print(dict_test)
-
+###删
 dict_test.pop('dd')
 print(dict_test)
