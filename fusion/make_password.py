bbf3eb4b819373e24cd2095787158ef02ad51fde
sunleoyao <sunleoyao@qq.com>
2019-02-01 00:19:30 +0800

    Update make_password.py

--- fusion/make_password.py
+++ fusion/make_password.py
@@ -1,55 +1,54 @@
 # -*- coding: utf-8 -*-
 import random
+import math
+
+how_many=6  #生产多少位的随机数
+how_many_change=3 #按照从小到大的顺序有多少位需要做映射
+log_path='C://Users//Administrator//Desktop//log//'  #日志路径，random.info为未处理随机数,change.info为映射后list
+
+def write_file(filename,what_write=[]):
+    filename=log_path+filename
+    with open(filename,'w') as file_object:
+        for temp in what_write:
+            file_object.write(temp)
+            file_object.write('\n')
 
 #产生1个随机数，个十百位数不同
 def make_num():
-    num=random.randint(1000, 9990)
-    no1=num//1000
-    no2=num//100%10
-    no3=num//10%10
-    no4=num%10
-    if((no1==no2)|(no2==no3)|(no3==no4)|(no2==no4)|(no1==no3)|(no1==no4)):
-        return make_num()
-    else:
-        return num
+    num_list=[]
+    num=random.randint((int(math.pow(10,how_many-1))), (int(math.pow(10,how_many)))-1)
+    for temp in str(num):
+        num_list.append(temp)
+    for i in range(0,len(num_list)):
+        for j in range(i+1,len(num_list)):
+            if num_list[i]==num_list[j]:
+                return make_num()
+    return num_list
 
 #按照要求产生x个随机数
-def make_num_list(x_temp):
+def how_num_list(n):
     list_temp=[]
-    x=int(x_temp)
-    for n in range(0,x):
-        temp=make_num()
-        list_temp.append(temp)
+    for n in range(n):
+        list_temp.append(change_char())
     return list_temp
 
-#按照要求，做对应关系
-def change_char(list_num_input):
+#按照要求，针对一组list做对应关系
+def change_char():
+    list_num_input=make_num()
     key_value={0:')',1:'!',2:'@',3:'#',4:'$',5:'%',6:'^',7:'&',8:'*',9:'('}
-    list_num=list_num_input[:]
-    list_num_long=len(list_num)
-    for n in range(0,list_num_long):
-        num=list_num[n]
-        no1=num//1000
-        no2=num//100%10
-        no3=num//10%10
-        no4=num%10
-        temp=[no1,no2,no3,no4]
-        temp_sort=temp[:]
-        temp_sort.sort()
-        small1=temp.index(temp_sort[0])
-        small2=temp.index(temp_sort[1])
-        small3=temp.index(temp_sort[2])
-        small4=temp.index(temp_sort[3])
-        temp[small1]=key_value[temp[small1]]
-        temp[small2]=key_value[temp[small2]]
-        temp[small3]=str(temp[small3])
-        temp[small4]=str(temp[small4])
-        list_num[n]=temp[0]+temp[1]+temp[2]+temp[3]
-    return(list_num_input,list_num)
+    temp=''
+    for i in list_num_input:
+        temp=temp+i
+    write_file('random.inf',temp)
+    print(list_num_input)
+    list_sort=list_num_input[:]
+    list_sort.sort()
+    for i in range(how_many_change):
+        temp=list_num_input.index(list_sort[i])
+        list_num_input[temp]=key_value[int(list_sort[i])]
+    return list_num_input
 
 if __name__=='__main__':
     chars_num_str=input("Please input how many number do you want?\n")
     chars_num=int(chars_num_str)
-    chars_num_nochange,chars_num_change=change_char(make_num_list(chars_num))
-    for n in range(0,chars_num):
-        print(str(chars_num_nochange[n])+':'+chars_num_change[n])
+    print(how_num_list(chars_num))
