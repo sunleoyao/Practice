d09db8582d1ad9747c93f31b22b4246a4725748b
sunleoyao <sunleoyao@qq.com>
2019-02-11 17:53:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,46 @@
-# -*- coding: utf-8 -*-
 import paramiko
-# 创建SSH对象
-ssh = paramiko.SSHClient()
-# 把要连接的机器添加到known_hosts文件中
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 连接服务器
-ssh.connect(hostname='192.168.122.134', port=22,
-            username='root', password='rootroot')
-# cmd = 'ls -l;ifconfig'       #多个命令用;隔开
-cmd = 'whoami'
-stdin, stdout, stderr = ssh.exec_command(cmd)
-print("stdin\n")
-print(stdin)
-print("stdout\n")
-print(stdout)
-print("stderr\n")
-print(stderr)
-result = stdout.read()
-if not result:
-    result = stderr.read()
-ssh.close()
-print(result.decode())
+import time
+
+def verification_ssh(host,username,password,port,root_pwd,cmd):
+    s=paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(hostname = host,port=int(port),username=username, password=password)
+    if username != 'root':
+        ssh = s.invoke_shell()
+        time.sleep(0.1)
+        #先判断提示符，然后下一步在开始发送命令，这样大部分机器就都不会出现问题
+        buff = ''
+        while not buff.endswith('$ '):
+            resp = ssh.recv(9999)
+            buff += resp.decode('utf8')
+            time.sleep(0.1)
+        print('获取登录后的提示符：%s' %buff)
+        ssh.send('export LANG=en_US.UTF-8 \n') #解决错误的关键，编码问题
+        ssh.send('export LANGUAGE=en \n')
+        ssh.send('su - \n')
+        buff = ""
+        while not buff.endswith('Password: '): #true
+            resp = ssh.recv(9999)
+            print(resp)
+            buff +=resp.decode('utf8')
+        print(buff)
+        ssh.send(root_pwd)
+        ssh.send('\n')
+        buff = ""
+        while not buff.endswith('# '):
+            resp = ssh.recv(9999)
+            print(resp)
+            buff +=resp.decode('utf8')
+        ssh.send('df -h') #放入要执行的命令
+        ssh.send('\n')
+        buff = ''
+        while not buff.endswith('# '):
+            resp = ssh.recv(9999).decode()
+            buff +=resp
+        print(buff)
+        result  = buff
+        s.close()
+
+if __name__ == "__main__":
+    verification_ssh('192.168.122.134', 'riil', 'riiladmin', '22', 'rootroot1', 'id')
