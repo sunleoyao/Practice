da9b96443a8cc1ea483ed2acb7a18065fb8736fa
sunleoyao <sunleoyao@qq.com>
2019-02-11 17:53:33 +0800

    Update try_right_password.py

--- fusion/try_right_password.py
+++ fusion/try_right_password.py
@@ -1,42 +1,64 @@
-# -*- coding: utf-8 -*-
 import paramiko
 import time
-# 服务器相关信息,下面输入你个人的用户名、密码、ip等信息
-#ips = ['192.168.122.134','192.168.122.134','192.168.122.134']
-ips = ['192.168.122.134']
-port =  22
-user = "riil"
-password = "riiladmin"
-root_password=['rootroot1','rootroot2','rootroot','rootroot3']
-#cmds=['whoami', 'pwd\n', 'df -h\n']
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 建立连接
-for ip in ips:
-    ssh.connect(ip,port,user,password)
-    chan=ssh.invoke_shell()
-    time.sleep(1)
-    output=''
-    chan.send('whoami'+'\n')
-    time.sleep(1)
-    output=chan.recv(2048)
-    print(output)
-    output=''
-    chan.send('su - root'+'\n')
-    time.sleep(1)
-    output=chan.recv(2048)
-    print(output)
-    output=''
-    chan.send('rootroot'+'\n')
-    time.sleep(1)
-    output=chan.recv(2048)
-    print(output)
-    output=''
-    chan.send('whoami'+'\n')
-    time.sleep(1)
-    output=chan.recv(2048)
-    print(output)
-    output=''
-    print(output)
-    chan.close()
+import sys
+
+def ssh_user(host,username,password,port):
+    s=paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        s.connect(hostname = host,port=int(port),username=username, password=password,timeout=1)
+    except Exception as a:
+        print('目标服务器异常!')
+        sys.exit(0)
+    if username != 'root':
+        ssh = s.invoke_shell()
+        time.sleep(0.1)
+        #先判断提示符，然后下一步在开始发送命令，这样大部分机器就都不会出现问题
+        buff = ''
+        while not buff.endswith('$ '):
+            resp = ssh.recv(9999)
+            buff += resp.decode('utf8')
+            time.sleep(0.1)
+        # print('获取登录后的提示符：%s' %buff)
+        ssh.send('export LANG=en_US.UTF-8 \n') #解决错误的关键，编码问题
+        ssh.send('export LANGUAGE=en \n')
+    print('Successful login in '+host+' !!!')
+    print('login as riil')
+    return ssh
+
+def ssh_su_root(ssh,root_pwd):
+    ssh.send('su - \n')
+    buff = ""
+    while not buff.endswith('Password: '): #true
+        resp = ssh.recv(9999)
+        # print(resp)
+        buff +=resp.decode('utf8')
+    # print(buff)
+    ssh.send(root_pwd)
+    ssh.send('\n')
+    buff = ""
+    while not buff.endswith('# '):
+        resp = ssh.recv(9999)
+        # print(resp)
+        buff +=resp.decode('utf8')
+    print('login as root')
+
+def ssh_root_cmd(ssh,root_cmd):
+    ssh.send(root_cmd) #放入要执行的命令
+    ssh.send('\n')
+    buff = ''
+    while not buff.endswith('# '):
+        resp = ssh.recv(9999).decode()
+        buff +=resp
+    print(buff)
+    result  = buff
+
+def ssh_close(ssh):
     ssh.close()
+
+if __name__ == "__main__":
+    a=ssh_user('192.168.122.134', 'riil', 'riiladmin', '22')
+    ssh_su_root(a,'rootroot')
+    ssh_root_cmd(a,'whoami')
+    ssh_close(a)
