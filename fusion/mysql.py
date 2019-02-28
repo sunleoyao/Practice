a91cda0c43be4fb21a322a9d1fb1be1ec9e552db
sunleoyao <sunleoyao@qq.com>
2019-02-28 18:22:58 +0800

    修改，未完成

--- fusion/mysql.py
+++ fusion/mysql.py
@@ -1,22 +1,24 @@
 import pymysql
+import sys
 
 class mysql(object):
+
     def __init__(self, ip, user_name, user_passwd, mysql_name):
         self.ip = ip
         self.user_name = user_name
         self.user_passwd = user_passwd
         self.mysql_name = mysql_name
-
-    def connect(self):
-        db = pymysql.connect(self.ip,self.user_name,self.user_passwd,self.mysql_name)
-        cursor = db.cursor()
-        return db,cursor
+        try:
+            self.db = pymysql.connect(self.ip,self.user_name,self.user_passwd,self.mysql_name)
+            self.cursor = self.db.cursor()
+        except Exception as a:
+            print('Please check mysql information!')
+            sys.exit(0)
     
-    def execute(self,cursor,sql):
-        cursor.execute(sql)
-        data=cursor.fetchone()
+    def excute(self,sql):
+        self.cursor.execute(sql)
+        data=self.cursor.fetchone()
         return data
 
-    def close(self,db):
-        db.close()
-        
+    def close(self):
+        self.db.close()
--- fusion/write_xls.py
+++ fusion/write_xls.py
@@ -1,4 +1,24 @@
-import mysql
+from mysql import mysql
 
 
-test=mysql('localhost','yao','yao','mysql')
+# calc_mysql=mysql('localhost','yao','yao','mysql')
+# calc_mysql.close()
+
+def read_file(path):
+    try:
+        f = open(path, 'r')
+        result=f.readlines()
+        return result
+    except:
+        print("Please check file path!")
+    finally:
+        if f:
+            f.close()
+
+def list_to_lists(list,split):
+    for i in list:
+        
+
+
+
+read_file('D:\\OneDrive\\code\\result.txt')
